749c157cfa43aaf8713cc6aa908b03ecc21e094d
Paul Elliott <paul@vizworkshop.com>
2020-08-16 15:44:14 -0700

    github editor

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Get this thing on netlify.
+          From github edirot.
         </p>
         <a
           className="App-link"
