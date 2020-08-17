1b69055f90a8071a593228ca030a444f578aab39
Paul Elliott <pelliott@theia.io>
2020-08-16 17:41:29 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Just a draft.
+          Envs are weird.
         </p>
         <a
           className="App-link"
