3a9bc92de84d9ba6edb7e200c4b7ce2810efb5c5
Paul Elliott <pelliott@theia.io>
2020-08-16 18:44:23 -0700

    works!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Envs are weird.
+          Envs are weird. really weiede
         </p>
         <a
           className="App-link"
