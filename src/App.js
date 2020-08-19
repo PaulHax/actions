805e9a1d6f8cd5996c43ab57d207c1a16816b0e4
Paul Elliott <pelliott@theia.io>
2020-08-18 17:47:45 -0700

    somehting new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Stage branch
+          Something new on Stage branch
         </p>
         <a
           className="App-link"
